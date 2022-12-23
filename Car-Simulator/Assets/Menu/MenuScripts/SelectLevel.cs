6c03d83eb40a085775757c5cd76aa5d90f31c0c5
Dmytro <dmytro.kravets@oa.edu.ua>
2022-12-23 13:12:07 +0200

    1

---
+++ Car-Simulator/Assets/Menu/MenuScripts/SelectLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    public void Map1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Map2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Map3()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
